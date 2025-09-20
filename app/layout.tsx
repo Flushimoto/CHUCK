f5b6f0ac881bd673207db11bab27ff29b4e40340
Flushimoto <njoni.prishtine@gmail.com>
2025-09-21 00:38:15 +0200

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Chuck Norris Coin ($CHUCK)",
+  description: "The only coin that can un-rug itself.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
